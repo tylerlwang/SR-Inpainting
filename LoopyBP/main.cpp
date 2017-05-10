2f388270a7c4aadfcc586772776fb662876b868c
tylerlwang <tyler_wang@brown.edu>
2017-05-10 17:21:34 -0400

    Modify LoopyBP

--- LoopyBP/main.cpp
+++ LoopyBP/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
-include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgcodecs/imgcodecs.hpp>
 #include <string>
 #include <vector>
