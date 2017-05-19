40c03c220152776cfebbc11ccb205e722b33f1bc
allenjguo <bestgjch@gmail.com>
2017-05-18 20:29:25 -0400

    Write weights to .txt file

--- Contour_Gb/berk.m
+++ Contour_Gb/berk.m
@@ -2,7 +2,7 @@
 % B = B{1};
 % linearIndices = sub2ind([size(original_image,1) size(original_image,2)],B(:,1),B(:,2));
 clear;
-original_image = imread('../Datasets/workingset/img.png');
+original_image = imread('../Datasets/Current/input.png');
 % mask = (original_image(:,:,2) == 255);
 fillColor = [0 255 0];
 mask = original_image(:,:,1)==fillColor(1) & ...
@@ -10,7 +10,7 @@ mask = original_image(:,:,1)==fillColor(1) & ...
 weights = zeros(13,1);
 
 for ind = 1:13
-    imgFile = ['../Datasets/workingset/' num2str(ind) '.png'];
+    imgFile = ['../Datasets/Current/' num2str(ind) '.png'];
     output_image = imread(imgFile);
     image_masked = cat(3,output_image(:,:,1).*uint8(mask),output_image(:,:,2).*uint8(mask),...
         output_image(:,:,3).*uint8(mask));
@@ -19,8 +19,9 @@ for ind = 1:13
     % edgeComponents(linearIndices) = 0;
     weights(ind) = nnz(edgeComponents) / nnz(mask);
 end
-
-
+fid = fopen('../Datasets/Current/weights.txt', 'wt');
+fprintf(fid,'%f\n',weights);
+fclose(fid);
 % output_image_2 = imread('5.png');
 % output_image_masked_2 = cat(3,output_image_2(:,:,1).*uint8(mask),output_image_2(:,:,2).*uint8(mask),...
 %     output_image_2(:,:,3).*uint8(mask));
